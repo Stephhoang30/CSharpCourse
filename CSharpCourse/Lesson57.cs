dceaef7433e7a16758e4ac08c965ceae0f5842c8
stephHoang30 <steph30forwork@gmail.com>
2024-09-09 11:46:36 +0700

    upload

---
+++ CSharpCourse/Lesson57.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourse
+{
+    class Lesson57
+    {
+        static void Main()
+        {
+            // định nghĩa một biểu thức lambda và gán biến action
+            var action = () => Console.WriteLine("Hello Ha Phuong");
+
+            action();
+
+            // câu lệnh lambda tìm giá trị max trong ba số
+            var findMax = (int a, int b, int c) =>
+            {
+                int max = a;
+                if (max < b)
+                {
+                    max = b;
+                }  
+                if (max < c)
+                {
+                    max = c;    
+                }
+                return max;
+            };  
+
+        }
+    }
+}
+
